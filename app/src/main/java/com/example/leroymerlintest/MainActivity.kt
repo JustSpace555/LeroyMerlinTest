1a020a1d60893ff1d83e3ceeb0dab9896016a68c
JustSpace555 <bigmishuk@gmail.com>
2021-04-19 16:16:52 +0300

    minor fixes for launch

--- app/src/main/java/com/example/leroymerlintest/MainActivity.kt
+++ app/src/main/java/com/example/leroymerlintest/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
@@ -15,12 +16,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.mainPageFragment))
+        setupActionBarWithNavController(navHostFragment.navController, appBarConfiguration)
+        navView.setupWithNavController(navHostFragment.navController)
+
+        supportActionBar?.hide()
     }
 }
